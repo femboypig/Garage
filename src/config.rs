1a20bea0f3c6ad54cec7e301ee5d13adf14aaac8
femboypig <harimassaa@gmail.com>
2026-06-09 12:50:42 +0400

    Refine Dark and Dracula theme color palettes to be highly polished and premium in config.rs

--- src/config.rs
+++ src/config.rs
@@ -155,130 +155,130 @@ impl Theme {
     pub fn dark() -> Self {
         Self {
             name: "Dark Theme".to_string(),
-            titlebar_bg: [0.11, 0.11, 0.11, 1.0],
-            titlebar_border: [0.18, 0.18, 0.18, 1.0],
-            titlebar_text: [0.85, 0.85, 0.85, 1.0],
-            titlebar_hover_bg: [0.22, 0.22, 0.22, 1.0],
-            titlebar_brand_text: [0.95, 0.95, 0.95, 1.0],
+            titlebar_bg: [0.035, 0.035, 0.043, 1.0], // zinc-950
+            titlebar_border: [0.153, 0.153, 0.165, 1.0], // zinc-800
+            titlebar_text: [0.8, 0.8, 0.8, 1.0],
+            titlebar_hover_bg: [0.153, 0.153, 0.165, 0.5],
+            titlebar_brand_text: [0.231, 0.514, 0.965, 1.0], // electric blue
             
-            sidebar_bg: [0.13, 0.13, 0.13, 1.0],
-            sidebar_border: [0.18, 0.18, 0.18, 1.0],
-            sidebar_text_dir: [0.8, 0.8, 0.85, 1.0],
-            sidebar_text_file: [0.75, 0.75, 0.75, 1.0],
-            sidebar_selected_bg: [0.26, 0.26, 0.26, 1.0],
-            sidebar_hover_bg: [0.2, 0.2, 0.2, 1.0],
+            sidebar_bg: [0.035, 0.035, 0.043, 1.0],
+            sidebar_border: [0.153, 0.153, 0.165, 1.0],
+            sidebar_text_dir: [0.957, 0.957, 0.961, 1.0],
+            sidebar_text_file: [0.635, 0.635, 0.663, 1.0], // zinc-400
+            sidebar_selected_bg: [0.153, 0.153, 0.165, 1.0],
+            sidebar_hover_bg: [0.153, 0.153, 0.165, 0.5],
             
-            tabbar_bg: [0.11, 0.11, 0.11, 1.0],
-            tabbar_border: [0.18, 0.18, 0.18, 1.0],
-            tab_active_bg: [0.18, 0.18, 0.18, 1.0],
-            tab_inactive_bg: [0.14, 0.14, 0.14, 1.0],
-            tab_text: [0.85, 0.85, 0.85, 1.0],
+            tabbar_bg: [0.035, 0.035, 0.043, 1.0],
+            tabbar_border: [0.153, 0.153, 0.165, 1.0],
+            tab_active_bg: [0.094, 0.094, 0.102, 1.0], // zinc-900
+            tab_inactive_bg: [0.035, 0.035, 0.043, 1.0],
+            tab_text: [0.957, 0.957, 0.961, 1.0],
             
-            breadcrumb_bg: [0.16, 0.16, 0.16, 1.0],
-            breadcrumb_border: [0.18, 0.18, 0.18, 1.0],
-            breadcrumb_text: [0.65, 0.65, 0.65, 1.0],
+            breadcrumb_bg: [0.094, 0.094, 0.102, 1.0],
+            breadcrumb_border: [0.153, 0.153, 0.165, 1.0],
+            breadcrumb_text: [0.443, 0.443, 0.478, 1.0], // zinc-500
             
-            editor_bg: [0.18, 0.18, 0.18, 1.0],
-            gutter_bg: [0.15, 0.15, 0.15, 1.0],
-            gutter_border: [0.22, 0.22, 0.22, 1.0],
-            active_line_bg: [0.22, 0.22, 0.22, 1.0],
-            line_number_active: [0.9, 0.9, 0.9, 1.0],
-            line_number_inactive: [0.5, 0.5, 0.5, 1.0],
-            selection_bg: [0.15, 0.35, 0.6, 0.5],
-            cursor_color: [0.0, 0.48, 0.8, 1.0],
+            editor_bg: [0.094, 0.094, 0.102, 1.0],
+            gutter_bg: [0.094, 0.094, 0.102, 1.0],
+            gutter_border: [0.153, 0.153, 0.165, 0.0],
+            active_line_bg: [0.153, 0.153, 0.165, 0.3],
+            line_number_active: [0.957, 0.957, 0.961, 1.0],
+            line_number_inactive: [0.322, 0.322, 0.353, 1.0], // zinc-700
+            selection_bg: [0.153, 0.153, 0.165, 0.8],
+            cursor_color: [0.231, 0.514, 0.965, 1.0], // electric blue
             
-            syntax_default: [0.85, 0.85, 0.85, 1.0],
-            syntax_keyword: [0.35, 0.61, 0.92, 1.0],
-            syntax_type: [0.3, 0.78, 0.63, 1.0],
-            syntax_number: [0.71, 0.8, 0.52, 1.0],
-            syntax_string: [0.81, 0.56, 0.44, 1.0],
-            syntax_comment: [0.4, 0.6, 0.4, 1.0],
-            syntax_attribute: [0.86, 0.86, 0.6, 1.0],
+            syntax_default: [0.957, 0.957, 0.961, 1.0],
+            syntax_keyword: [0.231, 0.514, 0.965, 1.0], // blue
+            syntax_type: [0.18, 0.8, 0.44, 1.0], // green
+            syntax_number: [0.95, 0.61, 0.07, 1.0], // orange
+            syntax_string: [0.9, 0.73, 0.09, 1.0], // yellow
+            syntax_comment: [0.443, 0.443, 0.478, 1.0], // comment grey
+            syntax_attribute: [0.61, 0.35, 0.71, 1.0], // purple
             
-            scrollbar_track: [0.16, 0.16, 0.16, 1.0],
-            scrollbar_border: [0.22, 0.22, 0.22, 1.0],
-            scrollbar_thumb: [0.3, 0.3, 0.3, 1.0],
-            scrollbar_thumb_hover: [0.4, 0.4, 0.4, 1.0],
+            scrollbar_track: [0.094, 0.094, 0.102, 1.0],
+            scrollbar_border: [0.153, 0.153, 0.165, 1.0],
+            scrollbar_thumb: [0.153, 0.153, 0.165, 0.6],
+            scrollbar_thumb_hover: [0.153, 0.153, 0.165, 0.9],
             
-            statusbar_bg: [0.11, 0.11, 0.11, 1.0],
-            statusbar_border: [0.18, 0.18, 0.18, 1.0],
-            statusbar_text: [0.75, 0.75, 0.75, 1.0],
+            statusbar_bg: [0.035, 0.035, 0.043, 1.0],
+            statusbar_border: [0.153, 0.153, 0.165, 1.0],
+            statusbar_text: [0.443, 0.443, 0.478, 1.0],
             
-            modal_bg: [0.15, 0.15, 0.15, 1.0],
-            modal_border: [0.25, 0.25, 0.25, 1.0],
-            modal_text_title: [0.95, 0.95, 0.95, 1.0],
-            modal_text_normal: [0.85, 0.85, 0.85, 1.0],
-            modal_text_muted: [0.55, 0.55, 0.55, 1.0],
-            button_bg: [0.22, 0.22, 0.22, 1.0],
-            button_hover_bg: [0.28, 0.28, 0.28, 1.0],
-            button_border: [0.3, 0.3, 0.3, 1.0],
-            button_text: [0.85, 0.85, 0.85, 1.0],
-            dropdown_hover_bg: [0.25, 0.25, 0.25, 1.0],
+            modal_bg: [0.094, 0.094, 0.102, 1.0],
+            modal_border: [0.153, 0.153, 0.165, 1.0],
+            modal_text_title: [0.957, 0.957, 0.961, 1.0],
+            modal_text_normal: [0.8, 0.8, 0.8, 1.0],
+            modal_text_muted: [0.443, 0.443, 0.478, 1.0],
+            button_bg: [0.153, 0.153, 0.165, 1.0],
+            button_hover_bg: [0.27, 0.27, 0.3, 1.0], // zinc-700
+            button_border: [0.153, 0.153, 0.165, 1.0],
+            button_text: [0.957, 0.957, 0.961, 1.0],
+            dropdown_hover_bg: [0.153, 0.153, 0.165, 1.0],
         }
     }
 
     pub fn dracula() -> Self {
         Self {
             name: "Dracula".to_string(),
-            titlebar_bg: [0.13, 0.14, 0.19, 1.0],
-            titlebar_border: [0.08, 0.085, 0.11, 1.0],
-            titlebar_text: [0.97, 0.97, 0.95, 1.0],
-            titlebar_hover_bg: [0.27, 0.28, 0.35, 1.0],
-            titlebar_brand_text: [1.0, 0.47, 0.78, 1.0],
+            titlebar_bg: [0.098, 0.102, 0.133, 1.0], // #191a21
+            titlebar_border: [0.204, 0.216, 0.275, 1.0], // #343746
+            titlebar_text: [0.973, 0.973, 0.949, 1.0], // #f8f8f2
+            titlebar_hover_bg: [0.267, 0.278, 0.353, 1.0], // #44475a
+            titlebar_brand_text: [1.0, 0.475, 0.776, 1.0], // #ff79c6 (pink)
             
-            sidebar_bg: [0.13, 0.14, 0.19, 1.0],
-            sidebar_border: [0.08, 0.085, 0.11, 1.0],
-            sidebar_text_dir: [0.74, 0.58, 0.98, 1.0],
-            sidebar_text_file: [0.97, 0.97, 0.95, 1.0],
-            sidebar_selected_bg: [0.27, 0.28, 0.35, 1.0],
-            sidebar_hover_bg: [0.27, 0.28, 0.35, 0.5],
+            sidebar_bg: [0.098, 0.102, 0.133, 1.0], // #191a21
+            sidebar_border: [0.204, 0.216, 0.275, 1.0], // #343746
+            sidebar_text_dir: [0.741, 0.576, 0.976, 1.0], // #bd93f9 (purple)
+            sidebar_text_file: [0.973, 0.973, 0.949, 1.0], // #f8f8f2
+            sidebar_selected_bg: [0.267, 0.278, 0.353, 1.0], // #44475a
+            sidebar_hover_bg: [0.267, 0.278, 0.353, 0.5],
             
-            tabbar_bg: [0.13, 0.14, 0.19, 1.0],
-            tabbar_border: [0.08, 0.085, 0.11, 1.0],
-            tab_active_bg: [0.16, 0.16, 0.21, 1.0],
-            tab_inactive_bg: [0.13, 0.14, 0.19, 1.0],
-            tab_text: [0.97, 0.97, 0.95, 1.0],
+            tabbar_bg: [0.098, 0.102, 0.133, 1.0], // #191a21
+            tabbar_border: [0.204, 0.216, 0.275, 1.0], // #343746
+            tab_active_bg: [0.157, 0.165, 0.212, 1.0], // #282a36 (editor bg)
+            tab_inactive_bg: [0.098, 0.102, 0.133, 1.0],
+            tab_text: [0.973, 0.973, 0.949, 1.0],
             
-            breadcrumb_bg: [0.16, 0.16, 0.21, 1.0],
-            breadcrumb_border: [0.08, 0.085, 0.11, 1.0],
-            breadcrumb_text: [0.55, 0.91, 0.99, 1.0],
+            breadcrumb_bg: [0.157, 0.165, 0.212, 1.0], // #282a36
+            breadcrumb_border: [0.204, 0.216, 0.275, 1.0], // #343746
+            breadcrumb_text: [0.545, 0.914, 0.992, 1.0], // #8be9fd (cyan)
             
-            editor_bg: [0.16, 0.16, 0.21, 1.0],
-            gutter_bg: [0.16, 0.16, 0.21, 1.0],
-            gutter_border: [0.08, 0.085, 0.11, 0.0],
-            active_line_bg: [0.27, 0.28, 0.35, 0.3],
-            line_number_active: [1.0, 0.72, 0.42, 1.0],
-            line_number_inactive: [0.38, 0.45, 0.64, 1.0],
-            selection_bg: [0.27, 0.28, 0.35, 0.6],
-            cursor_color: [0.97, 0.97, 0.95, 1.0],
+            editor_bg: [0.157, 0.165, 0.212, 1.0], // #282a36
+            gutter_bg: [0.157, 0.165, 0.212, 1.0], // #282a36
+            gutter_border: [0.204, 0.216, 0.275, 0.0],
+            active_line_bg: [0.267, 0.278, 0.353, 0.3], // #44475a with transparency
+            line_number_active: [1.0, 0.722, 0.424, 1.0], // #ffb86c (orange)
+            line_number_inactive: [0.384, 0.447, 0.643, 1.0], // #6272a4 (comment)
+            selection_bg: [0.267, 0.278, 0.353, 0.8],
+            cursor_color: [1.0, 0.475, 0.776, 1.0], // #ff79c6 (pink)
             
-            syntax_default: [0.97, 0.97, 0.95, 1.0],
-            syntax_keyword: [1.0, 0.47, 0.78, 1.0],
-            syntax_type: [0.55, 0.91, 0.99, 1.0],
-            syntax_number: [0.74, 0.58, 0.98, 1.0],
-            syntax_string: [0.95, 0.98, 0.55, 1.0],
-            syntax_comment: [0.38, 0.45, 0.64, 1.0],
-            syntax_attribute: [0.31, 0.98, 0.48, 1.0],
+            syntax_default: [0.973, 0.973, 0.949, 1.0], // #f8f8f2
+            syntax_keyword: [1.0, 0.475, 0.776, 1.0], // #ff79c6 (pink)
+            syntax_type: [0.545, 0.914, 0.992, 1.0], // #8be9fd (cyan)
+            syntax_number: [0.741, 0.576, 0.976, 1.0], // #bd93f9 (purple)
+            syntax_string: [0.945, 0.98, 0.549, 1.0], // #f1fa8c (yellow)
+            syntax_comment: [0.384, 0.447, 0.643, 1.0], // #6272a4
+            syntax_attribute: [0.314, 0.98, 0.482, 1.0], // #50fa7b (green)
             
-            scrollbar_track: [0.16, 0.16, 0.21, 1.0],
-            scrollbar_border: [0.08, 0.085, 0.11, 1.0],
-            scrollbar_thumb: [0.27, 0.28, 0.35, 0.6],
-            scrollbar_thumb_hover: [0.27, 0.28, 0.35, 0.9],
+            scrollbar_track: [0.157, 0.165, 0.212, 1.0],
+            scrollbar_border: [0.204, 0.216, 0.275, 1.0],
+            scrollbar_thumb: [0.267, 0.278, 0.353, 0.6],
+            scrollbar_thumb_hover: [0.267, 0.278, 0.353, 0.9],
             
-            statusbar_bg: [0.13, 0.14, 0.19, 1.0],
-            statusbar_border: [0.08, 0.085, 0.11, 1.0],
-            statusbar_text: [0.97, 0.97, 0.95, 1.0],
+            statusbar_bg: [0.098, 0.102, 0.133, 1.0], // #191a21
+            statusbar_border: [0.204, 0.216, 0.275, 1.0], // #343746
+            statusbar_text: [0.384, 0.447, 0.643, 1.0], // #6272a4
             
-            modal_bg: [0.16, 0.16, 0.21, 1.0],
-            modal_border: [0.74, 0.58, 0.98, 0.5],
-            modal_text_title: [1.0, 0.47, 0.78, 1.0],
-            modal_text_normal: [0.97, 0.97, 0.95, 1.0],
-            modal_text_muted: [0.38, 0.45, 0.64, 1.0],
-            button_bg: [0.27, 0.28, 0.35, 1.0],
-            button_hover_bg: [0.27, 0.28, 0.35, 0.7],
-            button_border: [0.74, 0.58, 0.98, 0.5],
-            button_text: [0.97, 0.97, 0.95, 1.0],
-            dropdown_hover_bg: [0.27, 0.28, 0.35, 1.0],
+            modal_bg: [0.157, 0.165, 0.212, 1.0], // #282a36
+            modal_border: [0.204, 0.216, 0.275, 1.0], // #343746
+            modal_text_title: [1.0, 0.475, 0.776, 1.0], // #ff79c6
+            modal_text_normal: [0.973, 0.973, 0.949, 1.0], // #f8f8f2
+            modal_text_muted: [0.384, 0.447, 0.643, 1.0], // #6272a4
+            button_bg: [0.267, 0.278, 0.353, 1.0], // #44475a
+            button_hover_bg: [0.384, 0.447, 0.643, 0.7],
+            button_border: [0.204, 0.216, 0.275, 1.0],
+            button_text: [0.973, 0.973, 0.949, 1.0],
+            dropdown_hover_bg: [0.267, 0.278, 0.353, 1.0],
         }
     }
 }
