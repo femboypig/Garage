e746e0aef85a871bb336da215a14b3d4e29c2ca0
femboypig <harimassaa@gmail.com>
2026-06-09 19:24:55 +0400

    renderer: update atlas glyph metrics scaling

--- src/renderer/atlas.rs
+++ src/renderer/atlas.rs
@@ -222,6 +222,8 @@ impl FontAtlas {
             "info" => include_str!("../../assets/icons/info.svg"),
             "settings" => include_str!("../../assets/icons/settings.svg"),
             "search" => include_str!("../../assets/icons/magnifying_glass.svg"),
+            "circle" => "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\"><circle cx=\"8\" cy=\"8\" r=\"7.0\" fill=\"currentColor\"/></svg>",
+            "close" => "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\"><path fill=\"currentColor\" d=\"M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.75.75 0 1 1 1.06 1.06L9.06 8l3.22 3.22a.75.75 0 1 1-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 0 1-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06z\"/></svg>",
             _ => {
                 log::warn!("Unknown embedded icon name: '{}'", icon_name);
                 return None;
