07474c43ea6da07f2f1a2e93c482d7af70d85976
femboypig <harimassaa@gmail.com>
2026-06-10 23:37:49 +0400

    Request low power preference to bypass slow graphics adapter scans on integrated GPUs

--- src/renderer/wgpu/mod.rs
+++ src/renderer/wgpu/mod.rs
@@ -87,7 +87,7 @@ impl GpuContext {
             };
 
             let mut adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::HighPerformance,
+                power_preference: wgpu::PowerPreference::LowPower,
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
             }).await;
@@ -95,7 +95,7 @@ impl GpuContext {
             if adapter.is_none() {
                 log::warn!("try_create (backend={:?}): request_adapter with compatible_surface returned None, retrying without compatible_surface...", backends);
                 adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-                    power_preference: wgpu::PowerPreference::HighPerformance,
+                    power_preference: wgpu::PowerPreference::LowPower,
                     compatible_surface: None,
                     force_fallback_adapter: false,
                 }).await;
