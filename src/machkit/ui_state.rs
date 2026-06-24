914afa3d45f94788243427ee88b24e5e6bcc6211
femboypig <harimassaa@gmail.com>
2026-06-24 23:43:05 +0400

    Fix clippy single-match warning in open_modal

--- src/machkit/ui_state.rs
+++ src/machkit/ui_state.rs
@@ -187,13 +187,10 @@ impl UiState {
 
     pub fn open_modal(&mut self, modal: ModalType) {
         self.active_modal = Some(modal);
-        match modal {
-            ModalType::CommandPalette => {
-                self.command_palette_query.clear();
-                self.command_palette_selected = 0;
-                self.command_palette_scroll = 0;
-            }
-            _ => {}
+        if modal == ModalType::CommandPalette {
+            self.command_palette_query.clear();
+            self.command_palette_selected = 0;
+            self.command_palette_scroll = 0;
         }
     }
 
